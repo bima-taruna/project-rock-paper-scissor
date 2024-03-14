8566dfceb9400b8409c87eb7222d8231c3cd8368
bima-taruna <bimataruna21@gmail.com>
2024-03-14 15:31:42 +0700

    add script

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+const playerScore = document.querySelector(".player-point");
+const compScore = document.querySelector(".comp-point");
+const command = document.querySelector(".command");
+const buttons = document.querySelectorAll("button")
+const body = document.querySelector("body");
+const h1 = document.querySelector("h1");
+
+let playerChoice = ""
+
+
+buttons.forEach((item) => {
+    item.addEventListener('click', (e) => {
+        playerChoice = item.className
+        playRound(playerChoice, getComputerChoice())
+        if (playerScore.textContent == 5) {
+            h1.textContent = "YOU WIN!"
+            command.remove()
+        } else if (compScore.textContent == 5) {
+            h1.textContent = "YOU LOSE!"
+            command.remove()
+        }
+    })
+})
+
+
 function getComputerChoice() {
     const compChoice = Math.floor(Math.random() * 3) + 1
     switch (compChoice) {
@@ -10,20 +35,20 @@ function getComputerChoice() {
     }
 }
 
-let playerChoice = "rock"
-
 function playRound(playerChoice, compChoice) {
     if (playerChoice == compChoice) {
-        return "Tie!"
+        alert("Tie!")
     } else if (playerChoice == "rock" && compChoice == "paper") {
-        return "You Lose! Paper beats Rock"
+        alert("You Lose! Paper beats Rock")
+        compScore.textContent++
     } else if (playerChoice == "paper" && compChoice == "scissor") {
-        return "You Lose! Scissor beats Paper"
+        alert("You Lose! Scissor beats Paper")
+        compScore.textContent++
     } else if (playerChoice == "scissor" && compChoice == "rock") {
-        return "You Lose! Rock beats Scissor"
+        alert("You Lose! Rock beats Scissor")
+        compScore.textContent++
     } else {
-        return `You Won! ${playerChoice} beats ${compChoice}`
+        alert(`You Won! ${playerChoice} beats ${compChoice}`)
+        playerScore.textContent++
     }
 }
-
-// console.log(playRound(playerChoice, getComputerChoice()))
